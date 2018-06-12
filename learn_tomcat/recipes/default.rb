5357f43844497a063345fea6db35917d65f6e206
Charitra <charliemaha7@gmail.com>
2018-06-12 10:16:42 +0000

    second updated recipe

--- learn_tomcat/recipes/default.rb
+++ learn_tomcat/recipes/default.rb
@@ -1,7 +1,9 @@
 #Cookbook Name :: learn_tomcat
 #Recipe:: default
-#Create a group name cool_group
+
 include_recipe 'java'
+
+#Create a group name cool_group
 group node['tomcat']['group_name']
 
 #Create a user name cool_user
@@ -14,13 +16,23 @@ end
 #Install Tomcat 8.0.47 to the default location
 learn_tomcat_install 'helloworld' do
    action :install
+   user node['tomcat']['user_name']
+   group node['tomcat']['group_name']
 end
 
-#restart tomcat if there is change in server.xml file
-cookbook_file node['tomcat']['server_file'] do
-    notifies :restart, 'learn_tomcat_run[helloworld]'
-end
+#Tomcat server configuration
+template node['tomcat']['server_file'] do
+   source 'server.conf.erb'
+   mode '0644'
+   owner node['tomcat']['user_name']
+   group node['tomcat']['group_name']
 
+#Tomcat catalina configuration
+template node['tomcat']['catalina_file'] do
+   source 'setenv.sh.erb'
+   mode '0755'
+   owner node['tomcat']['user_name']
+   group node['tomcat']['group_name']
 
 #copy sample.war file from source to remote location
 remote_file node['tomcat']['remote_file'] do
@@ -31,8 +43,13 @@ end
 
 
 #start and enable the helloworld tomcat service 
-learn_tomcat_run 'helloworld' do
-  action [:start, :enable]
+service 'helloworld' do
+  action [:enable, :start]
+  user node['tomcat']['user_name']
+  group node['tomcat']['group_name']
+  subscribes :restart, "template[#{node['tomcat']['catalina_file']}]", :delayed
+  subscribes :restart, "template[#{node['tomcat']['server_file']}]", :delayed
+
 end
 
 
