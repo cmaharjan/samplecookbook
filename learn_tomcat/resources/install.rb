fd95d5d13aa543bfb1b2618a909369425cc4bc06
Charitra <charliemaha7@gmail.com>
2018-06-12 10:21:54 +0000

    updated custom resource

--- learn_tomcat/resources/install.rb
+++ learn_tomcat/resources/install.rb
@@ -1,6 +1,4 @@
 property :state, String
 action :install do
    tarball_uri node['tomcat']['tomcat_package']
-   tomcat_user node['tomcat']['user_name']
-   tomcat_group node['tomcat']['group_name']
 end
