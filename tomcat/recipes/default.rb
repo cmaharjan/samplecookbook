0070b0f4f5bbd9f152654255cb10115ec81eb8b1
Charitra <charliemaha7@gmail.com>
2018-06-12 01:52:22 +0000

    updated recipe

--- tomcat/recipes/default.rb
+++ tomcat/recipes/default.rb
@@ -1,32 +1,32 @@
 #Cookbook Name :: tomcat
 #Recipe:: default
 #Create a group name cool_group
-group 'cool_group'
+group node['tomcat']['group_name']
 
 #Create a user name cool_user
-user 'cool_user' do
-   group 'cool_group'
+user node['tomcat']['user_name'] do
+   group node['tomcat']['group_name']
    system 'true'
    shell '/bin/bash'
 end
 
 #Install Tomcat 8.0.47 to the default location
 tomcat_install 'helloworld' do
-   tarball_uri 'http://archive.apache.org/dist/tomcat/tomcat-8/v8.0.47/bin/apache-tomcat-8.0.47.tar.gz'
-   tomcat_user 'cool_user'
-   tomcat_group 'cool_group'
+   tarball_uri node['tomcat']['tomcat_package']
+   tomcat_user node['tomcat']['user_name']
+   tomcat_group node['tomcat']['group_name']
 end
 
 #start and enable the helloworld tomcat service 
 
 tomcat_service 'helloworld' do
   action [:start, :enable]
-  tomcat_user 'cool_user'
-  tomcat_group 'cool_group'
+  tomcat_user node['tomcat']['user_name']
+  tomcat_group node['tomcat']['group_name']
 end
 
-remote_file '/opt/tomcat_helloworld/webapps/sample.war' do
-   owner 'cool_user'
+remote_file node['tomcat']['remote_file'] do
+   owner node['tomcat']['user_name']
    mode '0644'
-   source 'https://tomcat.apache.org/tomcat-7.0-doc/appdev/sample/sample.war'
+   source node['tomcat']['source_file']
 end
