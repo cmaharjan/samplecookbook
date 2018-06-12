8a1b390d507979982bb8b3daeb8478120711a551
Charitra <charliemaha7@gmail.com>
2018-06-12 08:52:39 +0000

    update and rearranged resource

--- learn_tomcat/recipes/default.rb
+++ learn_tomcat/recipes/default.rb
@@ -1,6 +1,7 @@
 #Cookbook Name :: learn_tomcat
 #Recipe:: default
 #Create a group name cool_group
+include_recipe 'java'
 group node['tomcat']['group_name']
 
 #Create a user name cool_user
@@ -15,15 +16,23 @@ learn_tomcat_install 'helloworld' do
    action :install
 end
 
-#start and enable the helloworld tomcat service 
-
-learn_tomcat_run 'helloworld' do
-  action [:start, :enable]
+#restart tomcat if there is change in server.xml file
+cookbook_file node['tomcat']['server_file'] do
+    notifies :restart, 'learn_tomcat_run[helloworld]'
 end
 
 
+#copy sample.war file from source to remote location
 remote_file node['tomcat']['remote_file'] do
    owner node['tomcat']['user_name']
    mode '0644'
    source node['tomcat']['source_file']
 end
+
+
+#start and enable the helloworld tomcat service 
+learn_tomcat_run 'helloworld' do
+  action [:start, :enable]
+end
+
+
