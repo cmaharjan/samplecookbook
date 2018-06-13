1713f0e3bb0aa6942e6c3b025e3595bc2e877c1d
Charitra <charliemaha7@gmail.com>
2018-06-13 23:26:57 +0000

    second updated attribute

--- learn_tomcat/attributes/default.rb
+++ learn_tomcat/attributes/default.rb
@@ -1,7 +1,5 @@
 #installation
-default['tomcat']['remote_file'] = '/opt/learn_tomcat_helloworld/webapps/sample.war'
-default['tomcat']['server_file'] = '/opt/learn_tomcat_helloworld/conf/server.xml'
-default['tomcat']['catalina_file'] = '/opt/learn_tomcat_helloworld/bin/setevn.sh'
+defalut['tomcat']['tomcat_home'] = '/opt/learn_tomcat'
 default['tomcat']['tomcat_package'] = 'http://archive.apache.org/dist/tomcat/tomcat-8/v8.0.47/bin/apache-tomcat-8.0.47.tar.gz'
 default['tomcat']['source_file'] = 'https://tomcat.apache.org/tomcat-7.0-doc/appdev/sample/sample.war'
 
