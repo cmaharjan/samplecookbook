346314790dee810d87248d826fa54dca2dd3d87f
Charitra <charliemaha7@gmail.com>
2018-06-13 23:29:01 +0000

    updated metadata

--- learn_tomcat/metadata.rb
+++ learn_tomcat/metadata.rb
@@ -4,4 +4,6 @@ maintainer_email 'charitra.maharjan1@gmail.com'
 license 'all_rights'
 description 'Installs/Configures learn_tomcat'
 long_description 'Installs/Configures learn_tomcat'
-version '0.1.0'
+version '0.1.1'
+
+depends 'ark', '~> 0.9.0'
