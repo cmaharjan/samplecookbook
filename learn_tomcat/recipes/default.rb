1f9bfead5436f5632e470f87557be5431a9747e3
Charitra <charliemaha7@gmail.com>
2018-06-13 23:28:05 +0000

    third updated recipe

--- learn_tomcat/recipes/default.rb
+++ learn_tomcat/recipes/default.rb
@@ -14,28 +14,32 @@ user node['tomcat']['user_name'] do
 end
 
 #Install Tomcat 8.0.47 to the default location
-learn_tomcat_install 'helloworld' do
-   action :install
-   user node['tomcat']['user_name']
+ark 'tomcat_serwar' do
+   url node['tomcat']['tomcat_package']
+   home_dir node['tomcat']['tomcat_home']
+   owner node['tomcat']['user_name']
    group node['tomcat']['group_name']
+   action :put
 end
 
 #Tomcat server configuration
-template node['tomcat']['server_file'] do
+template "#{node['tomcat']['tomcat_home']}/conf/server.xml" do
    source 'server.conf.erb'
    mode '0644'
    owner node['tomcat']['user_name']
    group node['tomcat']['group_name']
+end
 
 #Tomcat catalina configuration
-template node['tomcat']['catalina_file'] do
+template "#{node['tomcat']['tomcat_home']}/bin/setenv.sh" do
    source 'setenv.sh.erb'
    mode '0755'
    owner node['tomcat']['user_name']
    group node['tomcat']['group_name']
+end
 
 #copy sample.war file from source to remote location
-remote_file node['tomcat']['remote_file'] do
+remote_file "#{node['tomcat']['tomcat_home']}/webapps/sample.war" do
    owner node['tomcat']['user_name']
    mode '0644'
    source node['tomcat']['source_file']
@@ -43,9 +47,9 @@ end
 
 
 #start and enable the helloworld tomcat service 
-service 'helloworld' do
+service 'tomcat_serwar' do
   action [:enable, :start]
-  user node['tomcat']['user_name']
+  owner node['tomcat']['user_name']
   group node['tomcat']['group_name']
   subscribes :restart, "template[#{node['tomcat']['catalina_file']}]", :delayed
   subscribes :restart, "template[#{node['tomcat']['server_file']}]", :delayed
