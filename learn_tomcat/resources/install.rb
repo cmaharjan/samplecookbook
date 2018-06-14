370186001b70235bd1ca93837ec59dc64c0e7580
Charitra <charliemaha7@gmail.com>
2018-06-14 01:32:01 +0000

    remove resource folder

--- learn_tomcat/resources/install.rb
+++
@@ -1,4 +0,0 @@
-property :state, String
-action :install do
-   tarball_uri node['tomcat']['tomcat_package']
-end
--- learn_tomcat/resources/run.rb
+++
@@ -1,5 +0,0 @@
-property :state, String
-action [:start, :enable] do
-  tomcat_user node['tomcat']['user_name']
-  tomcat_group node['tomcat']['group_name']
-end
