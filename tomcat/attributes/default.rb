79505ac32e6c7858007e6f4ad49e0ed512900c6f
Charitra <charliemaha7@gmail.com>
2018-06-14 01:35:56 +0000

    remove previous tomcat cookbook

--- tomcat/attributes/default.rb
+++
@@ -1,5 +0,0 @@
-default['tomcat']['remote_file'] = '/opt/tomcat_helloworld/webapps/sample.war'
-default['tomcat']['tomcat_package'] = 'http://archive.apache.org/dist/tomcat/tomcat-8/v8.0.47/bin/apache-tomcat-8.0.47.tar.gz'
-default['tomcat']['source_file'] = 'https://tomcat.apache.org/tomcat-7.0-doc/appdev/sample/sample.war'
-default['tomcat']['group_name'] = 'cool_group'
-default ['tomcat']['user_name'] = 'cool_user'
