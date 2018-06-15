4553b19135fbf8c0b56da2699ea2ccdf41a1287e
Charitra <charliemaha7@gmail.com>
2018-06-15 04:19:17 +0000

    updated attribute

--- learn_tomcat/attributes/default.rb
+++ learn_tomcat/attributes/default.rb
@@ -1,5 +1,7 @@
 #installation
-defalut['tomcat']['tomcat_home'] = '/opt/learn_tomcat'
+defalut['tomcat']['tomcat_install_path'] = '/opt/learn_tomcat'
+defalut['tomcat']['tomcat_dirname'] = 'tomcat_serwar'
+defalut['tomcat']['tomcat_home'] = "#{node['tomcat']['tomcat_install_path']}/#{node['tomcat']['tomcat_dirname']}"
 default['tomcat']['tomcat_package'] = 'http://archive.apache.org/dist/tomcat/tomcat-8/v8.0.47/bin/apache-tomcat-8.0.47.tar.gz'
 default['tomcat']['source_file'] = 'https://tomcat.apache.org/tomcat-7.0-doc/appdev/sample/sample.war'
 
@@ -8,5 +10,5 @@ default['tomcat']['group_name'] = 'cool_group'
 default['tomcat']['user_name'] = 'cool_user'
 
 #configuration
-default['tomcat']['java-opts'] = 'Xms128m -Xmx512m'
+default['tomcat']['java-opts'] = "-Xmx512m"
 default['tomcat']['port'] = '8081'
