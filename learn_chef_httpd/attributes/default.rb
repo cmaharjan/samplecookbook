1b0e40dda3df156c9c906de4a1f809fac2eb9c83
Charitra <charliemaha7@gmail.com>
2018-06-16 02:42:03 +0000

    attributes added

---
+++ learn_chef_httpd/attributes/default.rb
@@ -0,0 +1,7 @@
+default['httpd']['user'] = 'web_admin'
+default['httpd']['group'] = 'web_admin'
+default['httpd']['port'] = '80'
+default['httpd']['tomcat_proxy'] = '127.28.128.5'
+default['httpd']['proxy_port'] = '8081'
+
+
