d07fbb2d40f078edff08ab6b989dcc7a4d271b38
Charitra <charliemaha7@gmail.com>
2018-06-18 21:20:36 +0000

    final attribute

--- learn_tomcat/attributes/default.rb
+++ learn_tomcat/attributes/default.rb
@@ -1,14 +1,14 @@
-#installation
-defalut['tomcat']['tomcat_install_path'] = '/opt/learn_tomcat'
-defalut['tomcat']['tomcat_dirname'] = 'tomcat_serwar'
-defalut['tomcat']['tomcat_home'] = "#{node['tomcat']['tomcat_install_path']}/#{node['tomcat']['tomcat_dirname']}"
+# installation
+default['tomcat']['tomcat_install_path'] = '/usr/local'
+default['tomcat']['tomcat_dirname'] = 'tomcat_serwar'
+default['tomcat']['tomcat_home'] = "#{node['tomcat']['tomcat_install_path']}/#{node['tomcat']['tomcat_dirname']}"
 default['tomcat']['tomcat_package'] = 'http://archive.apache.org/dist/tomcat/tomcat-8/v8.0.47/bin/apache-tomcat-8.0.47.tar.gz'
 default['tomcat']['source_file'] = 'https://tomcat.apache.org/tomcat-7.0-doc/appdev/sample/sample.war'
 
-#O.S related settings
+# O.S related settings
 default['tomcat']['group_name'] = 'cool_group'
 default['tomcat']['user_name'] = 'cool_user'
 
-#configuration
-default['tomcat']['java-opts'] = "-Xmx512m"
+# configuration
+default['tomcat']['java-opts'] = '-Xmx512m'
 default['tomcat']['port'] = '8081'
