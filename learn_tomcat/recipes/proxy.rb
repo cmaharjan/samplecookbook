d4d4256a4fcf68ba3985abf21c8ff042dccca032
Charitra <charliemaha7@gmail.com>
2018-06-27 18:15:52 +0000

    updated proxy recipe

--- learn_tomcat/recipes/proxy.rb
+++ learn_tomcat/recipes/proxy.rb
@@ -5,51 +5,53 @@
 # Creating a group
 group node['web']['group']
 
-#Creating a user
+# Creating a user
 user node['web']['user'] do
-   group node['web']['group']
-   system true
-   shell '/bin/bash'
+  group node['web']['group']
+  system true
+  shell '/bin/bash'
 end
 
 if node['webserver'] == 'apache'
-    # Installing apache package
-    package 'httpd'
+  # Installing apache package
+  package 'httpd'
 
-    # Enabling and starting apache package
-    service 'httpd' do
-      action [:enable, :start]
-    end
+  template '/var/www/html/index.html' do
+    source 'index.html.erb'
+    mode '0644'
+    owner node['web']['user']
+    group node['web']['group']
+  end
 
-    template '/var/www/html/index.html' do
-      source 'index.html.erb'
-      mode '0644'
-      owner node['web']['user']
-      group node['web']['group']
-    end
+  template '/etc/httpd/conf/httpd.conf' do
+    source 'httpd.conf.erb'
+    mode '0644'
+    owner node['web']['user']
+    group node['web']['group']
+  end
 
-    template '/etc/httpd/conf/httpd.conf' do
-      source 'httpd.conf.erb'
-      mode '0644'
-      owner node['web']['user']
-      group node['web']['group']
-    end
+  # Enabling and starting apache package
+  service 'httpd' do
+    action [:enable, :start]
+  end
+
+elsif node['webserver'] == 'nginx'
+  # include_recipe 'nginx'
+  package 'nginx'
+
+  template '/etc/nginx/nginx.conf' do
+    source 'nginx.conf.erb'
+    mode '0644'
+    owner node['web']['user']
+    group node['web']['group']
+    notifies :reload, 'service[nginx]', :delayed
+  end
+
+  service 'nginx' do
+    supports :status => true, :restart => true, :reload => true
+    action :enable
+  end
 
 else
-       #include_recipe 'nginx'
-    package 'nginx'
-
-    template '/etc/nginx/nginx.conf' do
-      source 'nginx.conf.erb'
-      mode '0644'
-      owner node['web']['user']
-      group node['web']['group']
-      notifies :reload, 'service[nginx]', :delayed
-   end
-
-   service 'nginx' do
-     supports :status => true, :restart => true, :reload => true
-     action :enable
-   end
-
+  'unexpected proxy provider'
 end
